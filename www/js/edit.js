2a2177f551b3874fbdfee24af095eb17305b3cf1
Michael <Mch.9975@gmail.com>
2016-02-09 17:27:11 +0700

    added icon dood

--- www/js/edit.js
+++ www/js/edit.js
@@ -1,15 +1,20 @@
 var canvas = new fabric.Canvas('c');
 var isRedoing = false;
 var h = [];
+var lastbrushsize;
 
 canvas.isDrawingMode = !canvas.isDrawingMode;
 canvas.setBackgroundColor('rgba(255,255,255,1)', canvas.renderAll.bind(canvas));
 
+lastbrushsize = canvas.freeDrawingBrush.width;
+
 function changeToBrush(){
+  canvas.freeDrawingBrush.width = lastbrushsize;
   canvas.freeDrawingBrush.color = 'rgba(0,0,0,1)';
 };
 
 function changeToEraser(){
+  canvas.freeDrawingBrush.width = 200;
   canvas.freeDrawingBrush.color = canvas.backgroundColor;
 };
 
