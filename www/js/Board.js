ac4a2dd13edb62b0bfd7dfd54b0ee865902ce634
Le_Ray <Le_Ray>
2016-02-29 16:03:28 +0700

    death of a thousand stars

--- www/js/Board.js
+++ www/js/Board.js
@@ -1,5 +1,29 @@
 var db = new PouchDB('http://localhost:5984/dbname');
 
+var addFunc = function(e) {
+
+  var cell = {
+      _id: this.id,
+      _rev: this.rev,
+      posX: this.style.left,
+      posY: this.style.top,
+      title: this.tit,
+      tags: this.tag,
+      canvas: this.can
+  };
+
+  db.put(cell, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a todo!');
+        location.reload();
+    }
+    else{
+      console.log(err);
+    }
+  });
+
+}
+
 db.allDocs({
   include_docs: true,
 }).then(function (result) {
@@ -15,12 +39,19 @@ console.log(rows);
     btn.style.border = "medium solid #000000";
     btn.style.width = "90px";
     btn.style.height = "90px";
+    btn.id = rows[k].doc._id;
+    btn.rev = rows[k].doc._rev;
+    btn.can = rows[k].doc.canvas;
+    btn.tag = rows[k].doc.tags;
+    btn.tit = rows[k].doc.title;
+    btn.addEventListener("mouseup", addFunc);
     var t = document.createTextNode(rows[k].doc.title);
     btn.appendChild(t);
     document.body.appendChild(btn);
     btn.className = "drag";
     $( ".drag" ).draggable();
-}
+  }
+
 
 }).catch(function (err) {
   console.log(err);
